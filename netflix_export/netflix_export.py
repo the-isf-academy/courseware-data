7ea20e5590f5451f3508305ea8938f50c9ad7244
emmaqbrown <eqbrown@isf.edu.hk>
2022-03-18 15:54:58 +0800

    separate tv show into columns

--- netflix_export/netflix_export.py
+++ netflix_export/netflix_export.py
@@ -3,17 +3,37 @@ import pandas as pd
 
 def parse_df(df):
     media_type = []
+    title_list = []
+    season = []
+    episode = []
+
 
     for title in netflix_df["Title"]:
         if 'Season' in title:
             media_type.append('tv')
+
+            title_split = title.split(":")
+
+            title_list.append(title_split[0])
+            season.append(title_split[1])
+            episode.append(title_split[2])
+
         elif title.count(':') > 1:
             media_type.append('other')
+            title_list.append(None)
+            season.append(None)
+            episode.append(None)
 
         else:
             media_type.append('movie')
+            title_list.append(None)
+            season.append(None)
+            episode.append(None)
     
     df['media_type'] = media_type
+    df['tv_show_title'] = title_list
+    df['season'] = season
+    df['episode'] = episode
     return df
 
 if __name__ == '__main__':
