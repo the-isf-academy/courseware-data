21fc6c025818b98a14117a2abdae01a246b5a85d
emma <eqbrown@isf.edu.hk>
2023-02-09 12:58:37 +0800

    separate date and time cols

--- whatsapp_export/export_whatsapp.py
+++ whatsapp_export/export_whatsapp.py
@@ -1,4 +1,4 @@
-from whatsapp_df import get_whatsapp_df
+from whatsapp_df import get_whatsapp_df, get_whatsapp_df_nozip
 from pathlib import Path
 import shutil
 import os
@@ -6,6 +6,8 @@ from pathlib import Path
 import tempfile
 import zipfile
 import pandas as pd
+import glob
+
 from simple_term_menu import TerminalMenu
 
 DATA_DIR = "data"
@@ -23,19 +25,32 @@ def get_chat_df(zipped_chat):
             df['chat_name'] = chat_name
             return df
 
+def get_chat_nozip_df(zipped_chat):
+    chat_file = glob.glob("data/*.txt")[0]
+    print(chat_file)
+    df = get_whatsapp_df_nozip(chat_file, DATETIME_FORMAT)
+    
+    chat_name = chat_file.split(" - ")[1].split(".")[0] 
+    df['chat_name'] = chat_name
+    return df
+
 def export_whatsapp_to_csv():
     chat_dfs_list = []
     data_dir = Path(DATA_DIR)
     for child in data_dir.iterdir():
+        # print(child)
         if child.suffix == ".zip":
                 df = get_chat_df(child)
                 chat_dfs_list.append(df)
+        # else:
+        #     df = get_chat_nozip_df(child)
+        #     chat_dfs_list.append(df)
     chat_dfs = pd.concat(chat_dfs_list)
     chat_dfs = chat_dfs.sort_values('date')
     chat_dfs.to_csv(EXPORT_DIR / "whatsapp.csv")
 
 if __name__ == '__main__':
-    datetime_dict = {"DD/MM/YYYY": "%d/%m/%Y, ","MM/DD/YYYY": "%m/%d/%Y, "}
+    datetime_dict = {"DD/MM/YYYY": "%d/%m/%Y","MM/DD/YYYY": "%m/%d/%Y"}
     date_format_menu = TerminalMenu(datetime_dict.keys(), title="Select date format of your chat data:")
     menu_entry_index = date_format_menu.show()
     DATETIME_FORMAT = datetime_dict[list(datetime_dict.keys())[menu_entry_index]]
--- whatsapp_export/whatsapp_df.py
+++ whatsapp_export/whatsapp_df.py
@@ -7,13 +7,18 @@ import yaml
 
 
 def get_whatsapp_df(chat_file, datetime_format="%m/%d/%Y, %X"):
+    print(chat_file)
     with chat_file.open() as f:
         lines = f.readlines()
         parsed_lines = []
         for line in lines:
+            line_valid = True
             line = line.strip('\u200e')
-            # print(line)
-            if line[0] != '[':
+            print(line)
+            if ' end-to-end encrypted' in line:
+                print('invalid', line)
+                line_valid = False
+            elif line[0] != '[':
                 # print(line)
                 same_msg = list(parsed_lines[-1])
                 same_msg[-1] += " " + line
@@ -23,7 +28,36 @@ def get_whatsapp_df(chat_file, datetime_format="%m/%d/%Y, %X"):
                 if parsed_line:
                     parsed_lines.append(parsed_line)
                 # print(parsed_line[0].split(',')[1])
-        df = pd.concat([pd.DataFrame({'date':[date.split(',')[0]],'time':[date.split(',')[1]], 'sender':[sender], 'message':[message]}) for date, sender, message in parsed_lines])
+        # print(parsed_line)
+        if line_valid:
+            df = pd.concat([pd.DataFrame({'date':[date],'time':[time],'sender':[sender], 'message':[message]}) for date, time, sender, message in parsed_lines])
+    df = df.sort_values('date')
+    return df
+
+def get_whatsapp_df_nozip(chat_file, datetime_format="%m/%d/%Y, %X"):
+    with open(chat_file) as f:
+        lines = f.readlines()
+        parsed_lines = []
+        for line in lines:
+            line = line.strip('\u200e')
+            # print(line)
+            
+            if ':' in line or len(line) ==0:
+                pass
+           
+            elif line[0] != '[':
+                if len(parsed_lines) > 0:
+                    same_msg = list(parsed_lines[-1])
+                    same_msg[-1] += " " + line
+                # print(same_msg)
+            else:
+                print(line)
+                parsed_line = parse_line(line, datetime_format)
+                if parsed_line:
+                    parsed_lines.append(parsed_line)
+                # print(parsed_line[0].split(',')[1])
+        print(parsed_lines)
+        df = pd.concat([pd.DataFrame({'date':[date], 'sender':[sender], 'message':[message]}) for date, sender, message in parsed_lines])
     df = df.sort_values('date')
     return df
 
@@ -34,19 +68,27 @@ def parse_line(line, datetime_format):
         split_line = line.split(':', 3)
         meta_data =  (':').join(split_line[:3])
         message = split_line[-1].strip().strip('\u200e')
-        date, name = meta_data.split(']')
+        date, name = meta_data.split('] ')
         date = date.strip('[')
-        # print(date)
-        if "am" or "pm" in date:
-            new_date_format = datetime_format + "%I:%M:%S %p"
-            date = datetime.strftime(
-                datetime.strptime(date, new_date_format),
-                    datetime_format + " %H:%M:%S")
-        else:
-            date = datetime.strptime(date, datetime_format + " %H:%M:%S")
+
+        date,time = date.split(',')
+        time = time.replace(' ','')
+        print('date',date)
+
+        if "am" in time or "pm" in time or 'AM' in time or 'PM' in time:
+            if 'pm' in time or 'PM' in time:
+                # print(len(time[:time.index(":")]))
+                # if len(time[:time.index(":")]) == 2:
+                    # print(time[:time.index(":")])
+                new_hour = int(time[:time.index(":")]) + 12
+                time = str(new_hour) + time[time.index(":"):]
+
+            time = time[:-2]
+    
+        print(name)
         name = name.strip()
         # print(date)
-        return (date, name, message)
+        return (date, time, name, message)
     else:
         return False
 
