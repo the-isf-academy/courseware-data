bcc49ae2a244261ee2c3db785059143d7a2b8a96
emma <eqbrown@isf.edu.hk>
2023-02-09 13:45:57 +0800

    improved parsing for differing titles and colons

--- netflix_export/netflix_export.py
+++ netflix_export/netflix_export.py
@@ -10,19 +10,52 @@ def parse_df(df):
 
     for title in netflix_df["Title"]:
         if 'Season' in title:
-            media_type.append('tv')
+            media_type.append('series')
 
             title_split = title.split(":")
+            if any(char.isdigit() for char in title_split[1]):
+                series_season_list = title_split[1].split(' ')
+
+            title_list.append(title_split[0])
+            season.append(series_season_list[-1])
+            episode.append(title_split[2])
+
+        elif title.count(':') == 1:
+            # print(title)
+            title_split = title.split(": ")
+            # print(title_split)
+            media_type.append('series')
+
+            title_list.append(title_split[0])
+            season.append(None)
+            episode.append(title_split[1])
+
+
+        elif title.count(':') > 1:
+            title_split = title.split(": ")
+            # print(title_split)
+            if any(char.isdigit() for char in title_split[1]):
+                series_season_list = title_split[1].split(' ')
+
+                print(title_split[1],series_season_list)
+
+            media_type.append('series')
+
+            title_list.append(title_split[0])
+            season.append(series_season_list[-1])
+            episode.append(None)
+
+        elif ':' in title:
+
+            media_type.append('series')
+
+            title_split = title.split(": ")
+            # print(title, title_split)
 
             title_list.append(title_split[0])
             season.append(title_split[1])
-            episode.append(title_split[2])
+            # episode.append(title_split[])
 
-        elif title.count(':') > 1:
-            media_type.append('other')
-            title_list.append(None)
-            season.append(None)
-            episode.append(None)
 
         else:
             media_type.append('movie')
@@ -31,7 +64,7 @@ def parse_df(df):
             episode.append(None)
     
     df['media_type'] = media_type
-    df['tv_show_title'] = title_list
+    df['series_title'] = title_list
     df['season'] = season
     df['episode'] = episode
     return df
